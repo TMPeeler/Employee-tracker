da12bef05f3f4515cb07cebf5bdcb72c380a4a98
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-04-24 11:58:13 -0700

    add base files and some basic schema to the database

---
+++ schema.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS departmentDB;
+CREATE DATABASE departmentDB;
+USE departmentDB;
+
+CREATE TABLE departmentDB (
+id INT PRIMARY KEY,
+name VARCHAR(30)
+
+
+
+);
+SELECT * FROM departmentDB;
+
+
+
+DROP DATABASE IF EXISTS roleDB;
+CREATE DATABASE roleDB;
+USE roleDB;
+
+CREATE TABLE roleDB (
+id INT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL 
+    
+);
+SELECT * FROM roleDB;
+
+
+
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+USE employeeDB;
+
+CREATE TABLE employeeDB (
+
+
+
+    
+);
+SELECT * FROM employeeDB;
